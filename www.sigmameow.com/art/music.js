13b0e912f1690cdefcb1f75d35369b0898e17a08
HowcanoeWang <howcanoewang@gmail.com>
2018-08-09 21:27:37 -0300

    Add switch music detail page
    1.  Add App folder
    2.  Music site support switch songs from MusicList
    3. Upload mp3 and pdf
    3. Beautify some CSS

---
+++ www.sigmameow.com/art/music.js
@@ -0,0 +1,43 @@
+// JavaScript Document
+var musicList = [
+  {
+    "id":"0",
+    "name":"动漫歌曲",
+    "intro":"动画中的BGM，时而轻快，时而忧伤，包含了绮丽多彩的情愫，牵动着剧情的发展。天空之城、Secret Base都是口琴的入门歌曲，吹奏的同时脑海中不断浮现出的剧情，倾听清亮的琴音，让时间静静流逝，让旋律在心中流淌。",
+    "cover":"img/harmonica_animation.png",
+    "song":[0,1,2]
+  }
+];
+
+var song = [
+  {
+    "id":"0",
+    "name":"Secret Base（动画《未闻花名》ed）",
+    "singer":"浩瀚猫",
+    "length":"05:36",
+    "cover":"img/SecretBase.png",
+    "mv":NaN,
+    "mp3":"audio/Secret Base.mp3",
+    "score":"score/secret_base.pdf"
+  },
+  {
+    "id":"1",
+    "name":"Innocent（动画《天空之城》主题曲）",
+    "singer":"浩瀚猫|飘零",
+    "length":"02:07",
+    "cover":"img/天空之城.jpg",
+    "mv":NaN,
+    "mp3":"audio/天空之城.mp3",
+    "score":"score/天空之城.pdf"
+  },
+  {
+    "id":"2",
+    "name":"青い栞（动画《未闻花名》op）",
+    "singer":"浩瀚猫",
+    "length":"05:40",
+    "cover":"img/蓝书签.jpg",
+    "mv":"https://www.bilibili.com",
+    "mp3":"audio/Blue Bookmark.mp3",
+    "score":"score/青い栞.pdf"
+  }
+];
