76b9e80d005f84b7dcce4f8682ff655e0428733c
HowcanoeWang <howcanoewang@gmail.com>
2020-03-29 14:50:44 +0800

    automatic homepage update

--- art/music_db.js
+++ art/music_db.js
@@ -5,7 +5,7 @@ var musicList = [
     "name":"动漫歌曲",
     "intro":"动画中的BGM，时而轻快，时而忧伤，包含了绮丽多彩的情愫，牵动着剧情的发展。天空之城、Secret Base都是口琴的入门歌曲，吹奏的同时脑海中不断浮现出的剧情，倾听清亮的琴音，让时间静静流逝，让旋律在心中流淌。",
     "cover":"img/harmonica_animation.png",
-    "song":[0,1,2]
+    "song":[0,1,2,3]
   }
 ];
 
@@ -13,8 +13,9 @@ var song = [
   {
     "id":"0",
     "name":"Secret Base（《未闻花名》ed）",
+    "date":"2018.07.17",
     "singer":"浩瀚猫",
-    "length":"05:36",
+    "time":"05:36",
     "cover":"img/SecretBase.jpg",
     "mv":NaN,
     "mp3":"audio/Secret Base.mp3",
@@ -23,8 +24,9 @@ var song = [
   {
     "id":"1",
     "name":"Innocent（《天空之城》主题曲）",
+    "date":"2018.06.16",
     "singer":"浩瀚猫|飘零",
-    "length":"02:07",
+    "time":"02:07",
     "cover":"img/天空之城.jpg",
     "mv":NaN,
     "mp3":"audio/天空之城.mp3",
@@ -33,11 +35,23 @@ var song = [
   {
     "id":"2",
     "name":"青い栞（《未闻花名》op）",
+    "date":"2018.09.21",
     "singer":"浩瀚猫",
-    "length":"05:40",
+    "time":"05:40",
     "cover":"img/蓝书签.jpg",
     "mv":"https://www.bilibili.com/video/av32135131",
     "mp3":"audio/Blue Bookmark.mp3",
     "score":"score/青い栞.pdf"
+  },
+  {
+    "id":"3",
+    "name":"いつも何度でも（《千与千寻》主题曲）",
+    "date":"2020.03.17",
+    "singer":"浩瀚猫",
+    "time":"02:24",
+    "cover":"img/千与千寻.jpg",
+    "mv":"https://www.bilibili.com/video/BV1H7411d7t9",
+    "mp3":"audio/Always with me.mp3",
+    "score":"score/千与千寻.pdf"
   }
 ];
