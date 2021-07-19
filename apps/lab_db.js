d543fa76bd093e52332f224b1708fdadb953ac9a
HowcanoeWang <howcanoewang@gmail.com>
2021-07-19 23:19:37 +0900

    add ipad room status

---
+++ apps/lab_db.js
@@ -0,0 +1,12 @@
+var db = {
+    "103": {
+        "room_title": "研究員室1",
+        "room_member": ["李", "王", "丸山", "荒谷"],
+        "color": "#4caf50",
+    },
+    "216": {
+        "room_title": "セミナー室",
+        "room_member": ["菅野", "奥田", "西田", "ワンモ", "Winnie", "Thum", "Tahir"],
+        "color": "#6f42c1",
+    },
+}
