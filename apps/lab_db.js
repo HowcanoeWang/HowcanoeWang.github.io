23d71e312e3d2781a9b8e0a7606f405ed6896f99
Howcanoe WANG <howcanoewang@gmail.com>
2021-07-20 16:10:16 +0900

    Update lab_db.js

--- apps/lab_db.js
+++ apps/lab_db.js
@@ -1,12 +1,12 @@
 var db = {
     "103": {
         "room_title": "研究員室1",
-        "room_member": ["李", "王", "丸山", "荒谷"],
+        "room_member": ["李", "王", "丸山", "荒谷", "森"],
         "color": "#4caf50",
     },
     "216": {
         "room_title": "セミナー室",
-        "room_member": ["菅野", "奥田", "西田", "ワンモ", "Winnie", "Thum", "Tahir"],
+        "room_member": ["菅野", "奥田", "西田", "Wangmo", "Winnie", "Thum", "Tahir", "江草", "イム"],
         "color": "#6f42c1",
     },
     "999": {
