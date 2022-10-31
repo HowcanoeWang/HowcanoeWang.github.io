562415667eedfecb57a5cacfa504ff8526591218
Howcanoe WANG <howcanoewang@gmail.com>
2022-10-31 10:34:21 +0900

    update lab_pad subtitle & Intl.

--- apps/lab_db.js
+++ apps/lab_db.js
@@ -1,17 +1,19 @@
 var db = {
     "103": {
         "room_title": "研究員室1",
-        "room_member": ["李", "王", "江", "Sylvain", "森", "細田", "高垣", "伊達"],
+        "room_member": ["李", "王", "江", "Sylvain"],
+        "room_subtitle": "",
         "color": "#4caf50",
     },
     "104": {
         "room_title": "研究員室2",
-        "room_subtitle": "Field Phenomics Research Laboratory",
+        "room_subtitle": "国際フィールドフェノミクス研究拠点<br/>Field Phenomics Research Laboratory",
         "room_member": ["石井", "兒玉", "赵", "張", "森永", "和田", "Irena"],
         "color": "#6f42c1",
     },
     "216":{
         "room_title": "セミナー室",
+        "room_subtitle": "",
         "room_member": ["菅野", "奥田", "西田", "Winnie", "Thum", "Tahir", "江草", "イム", "田口", "後藤", "八藤後"],
         "color": "#6f42c1",
     },
